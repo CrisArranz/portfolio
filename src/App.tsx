fa2168b58251dc3143c617b324ea75bde2462b03
CrisArranz <c.arranz.arevalo@gmail.com>
2024-04-10 23:52:15 +0200

    [PORTFOLIO]: update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { AboutMe, MyKnowledge } from "./components/sections"
 import { Header } from "./components/utils/Header";
 
 export const App = () => {
+  const [isDark, setisDark] = useState(false);
+
+  const handleClickDark = () => {
+    console.info(123123);
+    setisDark(prev => !prev);
+  }
+
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-full bg-dark-50 dark:bg-dark-950 bg-[linear-gradient(rgb(0,128,128,0.6),rgba(255,255,255,0))]">
-      <Header />
-      <AboutMe />
-      <MyKnowledge id="tecnologies" />
+    <div className={`${isDark ? "dark" : ""}`}>
+      <div className="absolute top-0 z-[-2] w-full dark:bg-black bg-[radial-gradient(ellipse_70%_70%_at_50%-15%,rgb(0,128,128,0.6),rgba(255,255,255,0))]">
+        <Header onClickDark={handleClickDark}/>
+        <AboutMe />
+        <MyKnowledge id="tecnologies" />
+      </div>
     </div>
   );
 }
