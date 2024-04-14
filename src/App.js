920c11832119db1bc16cfd8df66a60a40240236c
CrisArranz <c.arranz.arevalo@gmail.com>
2024-04-14 23:36:49 +0200

    [PORFOLIO] Add js files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { useState } from "react";
+import { AboutMe } from "./components/sections/about-me/about-me";
+import { Experience } from "./components/sections/experience/experience";
+import { Knowledge } from "./components/sections/knowledge/knowledge";
+import { Work } from "./components/sections/work/work";
+import { Header } from "./components/utils/header/header";
+export const App = () => {
+    const [isDark, setisDark] = useState(false);
+    const handleClickDark = () => {
+        setisDark(prev => !prev);
+    };
+    return (_jsx("div", { className: `${isDark ? "dark" : ""}`, children: _jsxs("div", { className: "absolute top-0 z-[-2] w-full dark:bg-black bg-[radial-gradient(ellipse_70%_70%_at_50%-15%,rgb(0,128,128,0.6),rgba(255,255,255,0))]", children: [_jsx(Header, { onClickDark: handleClickDark, isDark: isDark }), _jsx(AboutMe, {}), _jsx(Experience, { id: "experience" }), _jsx(Knowledge, { id: "tecnologies" }), _jsx(Work, { id: "projects" })] }) }));
+};
