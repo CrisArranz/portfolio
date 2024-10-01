64b3c245685a11f45fdde6812a68dfff49dfcf8a
CrisArranz <c.arranz.arevalo@gmail.com>
2024-10-01 20:40:15 +0200

    [PORTFOLIO]: Remove js elements

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './components/pages/home';
-export const App = () => {
-    return (_jsx(Router, { children: _jsx(Routes, { children: _jsx(Route, { path: "/", element: _jsx(Home, {}) }) }) }));
-};
--- src/components/utils/image/image.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const Image = ({ src, alt }) => {
-    return _jsx("img", { src: src, alt: alt, className: 'drop-shadow-sm w-[75vw] h-[75vw] md:w-[50vw] md:h-[50vw] lg:w-[20rem] lg:h-[15rem] lg:aspect-[1/4] xl:w-[25rem] xl:h-[25rem] xl:aspect-[9/3] rounded-full object-cover object-top shadow-lg dark:shadow-dark-900' });
-};
