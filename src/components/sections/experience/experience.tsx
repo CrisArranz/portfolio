ae72d9082711c8b67f9664055b1fe1decd3aaf55
CrisArranz <c.arranz.arevao@gmai.com>
2026-07-20 14:49:34 +0200

    Changes in portfolio

--- src/components/sections/experience/experience.tsx
+++ src/components/sections/experience/experience.tsx
@@ -11,7 +11,7 @@ export const Experience: React.FC<ExperienceProps> = ({ id }) => {
             <span className="absolute flex items-center justify-center w-6 h-6 bg-emerald-600 rounded-full -left-3 dark:bg-emerald-600">
               <svg aria-hidden="true" className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
             </span>
-            <Typography message="Software Engineer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}> 
+            <Typography message="Software Engineer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}>
               <Typography type="SPAN" message=" - " styles={{ className: "dark:text-white text-black font-normal" }} />
               <Typography type="SPAN" message="Openbank" styles={{ className: "text-sm text-fuchsia-800 dark:text-fuchsia-300/90" }} />
             </Typography>
@@ -22,18 +22,18 @@ export const Experience: React.FC<ExperienceProps> = ({ id }) => {
             <span className="absolute flex items-center justify-center w-6 h-6 bg-emerald-600 rounded-full -left-3 dark:bg-emerald-600">
               <svg aria-hidden="true" className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
             </span>
-            <Typography message="Teach Assistant" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}> 
+            <Typography message="Teach Assistant" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}>
               <Typography type="SPAN" message=" - " styles={{ className: "dark:text-white text-black font-normal" }} />
               <Typography type="SPAN" message="Ironhack" styles={{ className: "text-sm text-fuchsia-800 dark:text-fuchsia-300/90" }} />
             </Typography>
-            <time className="block mb-2 text-sm font-normal leading-none text-black dark:text-white">Noviembre 2022 - Presente</time>
+            <time className="block mb-2 text-sm font-normal leading-none text-black dark:text-white">Noviembre 2022 - Febrero 2024</time>
             <p className="mb-4 font-normal text-dark-700 dark:text-white text-base text-pretty">Ayudando al staff para los cursos en Remoto que se imparten en español. Explicación de teoria, resolución de problemas y ayuda emocional 😉</p>
           </li>
           <li className="mb-8 md:mb-6 ml-6">
             <span className="absolute flex items-center justify-center w-6 h-6 bg-emerald-600 rounded-full -left-3 dark:bg-emerald-600">
               <svg aria-hidden="true" className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
             </span>
-            <Typography message="Fullstack Developer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}> 
+            <Typography message="Fullstack Developer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}>
               <Typography type="SPAN" message=" - " styles={{ className: "dark:text-white text-black font-normal" }} />
               <Typography type="SPAN" message="Alvea" styles={{ className: "text-sm text-fuchsia-800 dark:text-fuchsia-300/90" }} />
             </Typography>
@@ -44,7 +44,7 @@ export const Experience: React.FC<ExperienceProps> = ({ id }) => {
             <span className="absolute flex items-center justify-center w-6 h-6 bg-emerald-600 rounded-full -left-3 dark:bg-emerald-600">
               <svg aria-hidden="true" className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
             </span>
-            <Typography message="Fullstack Developer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}> 
+            <Typography message="Fullstack Developer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}>
               <Typography type="SPAN" message=" - " styles={{ className: "dark:text-white text-black font-normal" }} />
               <Typography type="SPAN" message="Antartyca" styles={{ className: "text-sm text-fuchsia-800 dark:text-fuchsia-300/90" }} />
             </Typography>
@@ -55,7 +55,7 @@ export const Experience: React.FC<ExperienceProps> = ({ id }) => {
             <span className="absolute flex items-center justify-center w-6 h-6 bg-emerald-600 rounded-full -left-3 dark:bg-emerald-600">
               <svg aria-hidden="true" className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
             </span>
-            <Typography message="Fullstack Developer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}> 
+            <Typography message="Fullstack Developer" type="HEADING_3" styles={{ className: "flex items-center gap-2 mb-1 text-lg font-semibold text-black dark:text-white" }}>
               <Typography type="SPAN" message=" - " styles={{ className: "dark:text-white text-black font-normal" }} />
               <Typography type="SPAN" message="Steelmood" styles={{ className: "text-sm text-fuchsia-800 dark:text-fuchsia-300/90" }} />
             </Typography>
